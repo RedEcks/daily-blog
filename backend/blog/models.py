119c99cccfb993a931089eb69da1e9fb1a21122b
Xavier Dunkley <xavierxv18@gmail.com>
2023-11-26 17:43:59 -0500

    finish with models and now working on serializers

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -30,3 +30,33 @@ class BlogPost(models.Model):
     featured = models.BooleanField(default=True)
     date_created = models.DateTimeField(default=datetime.now, blank=True)
     
+def save(self, *args,**kwargs):
+    original_slug = slugify(self.title)
+    queryset= BlogPost.objects.all().filter(slug__iexact=original_slug).count()
+    
+    count=1
+    slug = original_slug
+    while(queryset):
+        slug = original_slug + '-' + str(count)
+        count += 1
+        queryset= BlogPost.objects.all().filter(slug__iexact=slug).count()
+        
+        
+    self.slug = slug
+    
+    if self.featured:
+        try:
+            temp = BlogPost.objects.get(featured=True)
+            if self != temp:
+                temp.featured = False
+                temp.save()
+        except BlogPost.DoesNotExist:
+            pass
+    
+    
+    super(BlogPost, self).save(*args, **kwargs)
+    
+def __str__(self):
+    return self.title
+
+    
